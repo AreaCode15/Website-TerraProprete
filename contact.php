09a8a8666a90934cb38f4e519bdd5dc7907f19d4
Area Code <87765067+AreaCode15@users.noreply.github.com>
2022-08-28 17:56:47 +0200

    add content

--- contact.php
+++ contact.php
@@ -11,9 +11,34 @@ $mailheader = "From:".$name."<".$email.">\r\n";
 
 $recipient = "contact@terra-multiservices.fr";
 
-mail($recipient, $subject,$email, $message, $mailheader)
+mail($recipient, $subject, $message, $mailheader)
 or die("error!");
 
-echo"Message envoyé avec succès!";
+echo '
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link href="assets/img/logo.png" rel="icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+    <center><h1><br /><br /><br />Merci de nous avoir contactez.<br /> Nous vous répondrons dans les plus bref délais.</h1>
+    <p class="back"><br /><br />Revenir à <a href="index.html">l accueil</a></p></center>
+</div>
+
+</body>
+
+</html>
+
+';
 
 ?>
